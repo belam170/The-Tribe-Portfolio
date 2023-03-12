93cc897889c55f739af9ea420f4adf3bd43ada67
belammuia <belammuia0@gmail.com>
2023-03-12 20:28:29 +0300

    MY FIRST PORTFOLIO

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  
+  Particles.init({
+    selector: ".background",
+    maxParticle: 120,
+    speed: 0.5,
+    connectParticles: true,
+    color: "#FFFFFF",
+  });
+};
